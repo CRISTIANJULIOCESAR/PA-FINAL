f206b8747f7cfa5b52a20c38ae26c33d6f18c464
Cristian Padron Manrique <inu_juliocesar@hotmail.com.com>
2025-01-15 15:17:34 -0600

    Actualización: cambios en plantillas y nuevos entregables

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # Ejecutar la app en modo debug (solo para desarrollo)
+    # Iniciar en modo debug para desarrollo
     app.run(debug=True)
